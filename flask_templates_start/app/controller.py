1af1cac209da978d2f50018d2255ab392cfd6f39
Neale Johnston <neale@team-johnston.com>
2020-10-29 14:32:30 +0000

    End of lesson - form working

--- flask_templates_start/app/controller.py
+++ flask_templates_start/app/controller.py
@@ -1,5 +1,19 @@
 from app import app
+from app.models.task import *
+from flask import render_template, request
+from app.models.todo_list import add_new_task, tasks
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html", title="Home", tasks=tasks)
+
+
+@app.route("/add-task", methods=["POST"])
+def add_task():
+    task_title = request.form["title"]
+    task_description = request.form["description"]
+    new_task = Task(task_title, task_description, False)
+    add_new_task(new_task)
+    print(request.form)
+    return render_template("index.html", title="Home", tasks=tasks)
